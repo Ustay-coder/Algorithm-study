fd6820cad6b8aa129d5286d6b409720cf5830ad4
김지안 <gimjian@gimjian-ui-Macmini.local>
2025-12-24 11:44:10 +0900

    sol: 4949

--- BOJ/stack/4949/main.py
+++ BOJ/stack/4949/main.py
@@ -1,9 +1,42 @@
 import sys
 
 
-def solve():
-    pass
+def solve(sentence):
+    stack = []
+    for char in sentence:
+        if char == "(" or char == "[":
+            stack.append(char) 
+        elif char == ")":
+            # 만약 비어 있을 경우, 그냥 더한다.
+            if(len(stack) == 0):
+                stack.append(char)
+                continue 
+            top = stack[-1]
+            if top == "(":
+                stack.pop()
+            else:
+                stack.append(char)
+        elif char == "]":
+            # 만약 비어 있을 경우, 그냥 더한다.
+            if(len(stack) == 0):
+                stack.append(char)
+                continue 
+            top = stack[-1]
+            if top == "[":
+                stack.pop()
+            else:
+                stack.append(char)
+        else:
+            continue 
+    if len(stack) == 0:
+        print("yes")
+    else:
+        print("no")
 
 
 if __name__ == "__main__":
-    solve()
+    while(True):
+        text = input()
+        if text == ".":
+            break 
+        solve(text)
