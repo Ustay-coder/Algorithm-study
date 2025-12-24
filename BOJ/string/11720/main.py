cd01a0da72d72178463ec537f358d0834a63aded
김지안 <gimjian@gimjian-ui-Macmini.local>
2025-12-24 11:07:38 +0900

    sol : 11720

--- BOJ/string/11720/main.py
+++ BOJ/string/11720/main.py
@@ -1,9 +1,15 @@
 import sys
 
 
-def solve():
-    pass
+def solve(N, numbers):
+
+    sum = 0
+    for number in numbers:
+        sum += int(number)
+    print(sum)
 
 
 if __name__ == "__main__":
-    solve()
+    N = int(input())
+    numbers = input()
+    solve(N, numbers)
